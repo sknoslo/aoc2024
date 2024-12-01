4805df195be8b0be6d1bbec740b39570b3d74c33
Steffen Olson <sknoslo@gmail.com>
2024-12-01 09:35:46 -0500

    day01

--- day01/main.go
+++ day01/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log"
 	"sknoslo/aoc2024/utils"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 var input string
@@ -13,7 +16,7 @@ func init() {
 		log.Fatal(err)
 	}
 
-	input = in
+	input = strings.TrimSpace(in)
 }
 
 func main() {
@@ -21,10 +24,69 @@ func main() {
 	utils.Run(2, parttwo)
 }
 
+func atoi(in string) int {
+	i, err := strconv.Atoi(in)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return i
+}
+
+func parseList() ([]int, []int) {
+	lines := strings.Split(input, "\n")
+	l, r := make([]int, len(lines)), make([]int, len(lines))
+
+	for i, line := range lines {
+		nums := strings.Fields(line)
+
+		l[i] = atoi(nums[0])
+		r[i] = atoi(nums[1])
+	}
+
+	return l, r
+}
+
+func absDiff(a, b int) int {
+	if a > b {
+		return a - b
+	}
+
+	return b - a
+}
+
 func partone() string {
-	return "incomplete"
+	l, r := parseList()
+
+	slices.Sort(l)
+	slices.Sort(r)
+
+	sum := 0
+
+	for i, a := range l {
+		b := r[i]
+
+		sum += absDiff(a, b)
+	}
+
+	return strconv.Itoa(sum)
 }
 
 func parttwo() string {
-	return "incomplete"
+	l, r := parseList()
+
+	rc := make(map[int]int, len(r))
+
+	for _, v := range r {
+		rc[v]++
+	}
+
+	sum := 0
+	for _, v := range l {
+		if c, ok := rc[v]; ok {
+			sum += v * c
+		}
+	}
+
+	return strconv.Itoa(sum)
 }
